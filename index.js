96479d7b9ae09f8a11607a9d89a770fa9327b351
Jeffrey Neer <darkshadow10120@gmail.com>
2023-01-17 11:42:34 -0600

    Building simple webhook router:

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 const express = require('express')
+const config = require('./config.json');
+const { Webhook } = require('discord-webhook-node');
+const hook = new Webhook(config.discordWebhook);
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(express.json())
+
+app.post('/', (req, res) => {
+
+    const gameName = req.body.value1;
+    const userName = req.body.value2;
+    const turnNumber = req.body.value3;
+
+    const discordName = lookupDiscordId(userName);
+
+    hook.send("Its your turn <@" + discordName +">! \nGame name: " + gameName + "\nTurn number: " + turnNumber);
+
+    res.send();
 })
 
 app.listen(port, () => {
+  console.log(config);
   console.log(`Example app listening on port ${port}`)
 })
+
+
+function lookupDiscordId(civUsername) {
+    let retrievedName = civUsername;
+    config.userNameMappings.forEach((element) => {
+        if (element.civName === civUsername) {
+            retrievedName = element.discordId;
+        }
+    });
+    return retrievedName;
+}
